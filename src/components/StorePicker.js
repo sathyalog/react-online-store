3716141b2d92c75f5186004eeaef1ba20d3a100d
svakacharla@sapient.com <svakacharla@sapient.com>
2018-07-07 23:05:54 +0530

    basic setup

---
+++ src/components/StorePicker.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+class StorePicker extends React.Component {
+    render() {
+        return(
+            <React.Fragment>
+            { /* Use React.Fragment instead of div to avoid unnecessary elements on DOM */ }
+                <form className="store-selector">
+                    <h2>
+                        Please Enter A Store
+                    </h2>
+                    <input type="text" required placeholder="Store Name"/>
+                    <button type="submit">Visit Store >> </button>
+                </form>
+            </React.Fragment>
+        );
+    }
+}
+
+export default StorePicker;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { render } from 'react-dom';
+import StorePicker from './components/StorePicker'
+
+
+render(<StorePicker/>,document.querySelector('#main'));
+
