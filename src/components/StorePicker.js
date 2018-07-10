8d84f6e7d60856b8972a878a9e9f147c45b52572
svakacharla@sapient.com <sathya.vakacharla@gmail.com>
2018-07-10 20:23:40 +0530

    on homepage load, get some store name from helpers is configured

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
     render() {
@@ -9,7 +10,7 @@ class StorePicker extends React.Component {
                     <h2>
                         Please Enter A Store
                     </h2>
-                    <input type="text" required placeholder="Store Name"/>
+                    <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
                     <button type="submit">Visit Store >> </button>
                 </form>
             </React.Fragment>
