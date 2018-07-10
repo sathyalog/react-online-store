38d9cdfe28cc9107c0d1304044fb30550364b07f
svakacharla@sapient.com <sathya.vakacharla@gmail.com>
2018-07-10 22:31:02 +0530

    events,refs and this binding

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,15 +2,26 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+    myInput = React.createRef();
+    
+    goToStore = (event) => {
+        event.preventDefault();
+        console.log(this);
+    }
+    
     render() {
         return(
             <React.Fragment>
             { /* Use React.Fragment instead of div to avoid unnecessary elements on DOM */ }
-                <form className="store-selector">
+                <form className="store-selector" onSubmit={this.goToStore}>
                     <h2>
                         Please Enter A Store
                     </h2>
-                    <input type="text" required placeholder="Store Name" defaultValue={getFunName()}/>
+                    <input type="text"
+                    ref={this.myInput} 
+                    placeholder="Store Name" 
+                    defaultValue={getFunName()} 
+                    required/>
                     <button type="submit">Visit Store >> </button>
                 </form>
             </React.Fragment>
